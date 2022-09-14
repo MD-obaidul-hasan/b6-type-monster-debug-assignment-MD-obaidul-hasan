5cd5378df7b1a720cfb26fd2aeb3759de787f7a6
MD-obaidul-hasan <md.obaidulhasanlimu@gmail.com>
2022-09-15 02:50:36 +0600

    time intiger

--- script.js
+++ script.js
@@ -134,6 +134,6 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
-}, 1000);
+  document.getElementById("show-time").innerHTML = `${startTime   ? parseInt(timeSpent) : 0} seconds`;
+  // document.getElementById("show-time").innerHTML = `${startTime   ? timeSpent.tofixed(0) : 0} seconds`;
+},1000 );
